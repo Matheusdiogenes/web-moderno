37d29a6d452fbea431d86cc12c8dd447ee0ea8e7
Matheus Diogenes <matheusdiogenes98@gmail.com>
2020-05-01 14:55:16 -0300

    começa a aula 46

--- exercicios-web/fundamentos/41_destructuring1.js
+++ exercicios-web/fundamentos/41_destructuring1.js
@@ -8,18 +8,19 @@ const pessoa = {
         numero: 1000
     }
 }
-
-const { nome, idade } = pessoa
+//destruturing
+const { nome, idade } = pessoa //pegue nome e idade de pessoa
 console.log(nome, idade)
 
-const { nome: n, idade: i } = pessoa
+const { nome: n, idade: i } = pessoa //pegue nome e idade de pessoa e atribua na variável n & i
 console.log(n, i)
 
 const { sobrenome, bemHumorada = true } = pessoa
 console.log(sobrenome, bemHumorada)
 
-const { endereco: { logradouro, numero, cep } } = pessoa
+const { endereco: { logradouro, numero, cep } } = pessoa 
+
 console.log(logradouro, numero, cep)
 
-const { conta: { ag, num } } = pessoa
+const { conta: { ag, num } } = pessoa //não tente acessar coisas que não existe
 console.log(ag, num)
