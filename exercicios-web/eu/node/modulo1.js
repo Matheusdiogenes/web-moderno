e62ac010fc5ed8e7c7ed972dabf73d02befaf435
Matheus Diogenes <matheusdiogenes98@gmail.com>
2020-05-30 22:37:55 -0300

    revisando nodejs

---
+++ exercicios-web/eu/node/modulo1.js
@@ -0,0 +1,9 @@
+module.exports = {
+    teste(){
+        return "sou de outro arquivo"
+    },
+    soma = (a,b) => {return a+b},
+    multiplica = (a,b) => {return a*b},
+    subtraia = (a,b) => {return a-b}
+
+}
---
+++ exercicios-web/eu/node/moduloPrincipal.js
@@ -0,0 +1,4 @@
+const modulo1 = require('./modulo1.js')
+
+a = modulo1.soma(10,2)
+console.log(a);
