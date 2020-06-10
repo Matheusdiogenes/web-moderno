02a790059bc63ac96733d0a4f0820a75f17a981c
Matheus Diogenes <matheusdiogenes98@gmail.com>
2020-06-10 15:11:56 -0300

    seção 11

---
+++ exercicios-web/fundamentos/variaveisEConstantes_15.js
@@ -0,0 +1,18 @@
+//Maneiras de declarar variaveis 
+//Regra geral: crie variavel usando let. O let não permite que a variavel seja redeclarada, já o var sim
+var a = 3
+let b = 4
+
+var a = 30
+b = 40
+
+console.log(a, b)
+
+a = 300
+b = 400
+
+console.log(a, b)
+
+const c = 5
+// c = 50
+console.log(c)
