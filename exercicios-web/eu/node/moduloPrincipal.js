82782c45db0e77f565f43eb0ffad85034ae59f3e
Matheus Diogenes <matheusdiogenes98@gmail.com>
2020-06-15 20:03:49 -0300

    deletando pastas

--- exercicios-web/eu/node/moduloPrincipal.js
+++
@@ -1,4 +0,0 @@
-const modulo1 = require('./modulo1.js')
-
-a = modulo1.soma(10,2)
-console.log(a);
---
+++ js/loops.js
@@ -0,0 +1,9 @@
+const times = ['Flamengo', 'Barcelona', 'Santos']
+
+for (const key in times) {
+    console.log(key);        
+}
+
+for (const iterator of times) {
+    console.log(iterator);    
+}
