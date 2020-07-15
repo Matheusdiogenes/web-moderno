1fdf63919aeb9e828f865b8dfa2e12e6960b1617
Matheus Diogenes <matheusdiogenes98@gmail.com>
2020-07-15 09:27:33 -0300

    bd

---
+++ exercicios-web/banco-relacional/excluirEstados.sql
@@ -0,0 +1,8 @@
+delete from `estados`
+where sigla = 'MV'
+
+
+delete from `estados`
+where id >= 1000
+
+select * from `estados`
---
+++ exercicios-web/banco-relacional/inserirNovoEstado.sql
@@ -0,0 +1,9 @@
+insert into estados (id, nome, sigla, regiao, populacao)
+values (1000, 'Novo', 'NV', 'Sul', 2.54)
+
+
+insert into estados (nome, sigla, regiao, populacao)
+values ('Mais Novo', 'MV', 'Norte', 2.51)
+
+-- o AUTO_INCREMENT vai sempre pra frente
+select * from `estados`
